e13f74312bd140b9e00028547508114c62ab5b5c
loyeh <asadi.loyeh@gmail.com>
2023-04-28 20:04:26 +0330

    making blocks

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const blockArr = [];
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
 console.log(ctx);
 
 // window.addEventListener("resize", () => {
 // 	canvas.width = window.innerWidth;
 // 	canvas.height = window.innerHeight;
+// 	blockArr.forEach((position) => {
+// 		blockBox(position.x, position.y);
+// 	});
 // });
 
 function blockBox(x, y) {
 	ctx.fillStyle = "dodgerblue";
 	ctx.beginPath();
-	ctx.fillRect(x, y, 70, 25);
+	ctx.fillRect(x, y, 70, 15);
 }
 
 for (let i = 0; i < 9; i++) {
 	for (let j = 0; j < 5; j++) {
-		let x = 42 + 80 * i;
-		let y = 50 + 38 * j;
+		let x = 45 + 80 * i;
+		let y = 50 + 25 * j;
 		const position = { x, y };
 		blockArr.push(position);
 	}
