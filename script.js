71bd56cda33264df1fd484f498d224b20d4ea630
loyeh <asadi.loyeh@gmail.com>
2023-04-28 23:45:19 +0330

    making code better

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-const blockArr = [];
 // canvas.width = window.innerWidth;
 // canvas.height = window.innerHeight;
 
@@ -14,21 +13,31 @@ console.log(ctx);
 // 	});
 // });
 
-function blockBox(x, y) {
-	ctx.fillStyle = "dodgerblue";
+function blockBox(x, y, dx, dy, color) {
+	ctx.fillStyle = color;
 	ctx.beginPath();
-	ctx.fillRect(x, y, 70, 15);
+	ctx.fillRect(x, y, dx, dy);
+}
+function blockArrayMaker(row, column) {
+	const blockArr = [];
+	for (let i = 0; i < row; i++) {
+		for (let j = 0; j < column; j++) {
+			let x = 35 + 60 * i;
+			let y = 50 + 25 * j;
+			let dx = 50;
+			let dy = 15;
+			const block = { x, y, dx, dy };
+			blockArr.push(block);
+		}
+	}
+	return blockArr;
 }
 
-for (let i = 0; i < 9; i++) {
-	for (let j = 0; j < 5; j++) {
-		let x = 45 + 80 * i;
-		let y = 50 + 25 * j;
-		const position = { x, y };
-		blockArr.push(position);
-	}
-}
-console.log(blockArr);
-blockArr.forEach((position) => {
-	blockBox(position.x, position.y);
+const blockArr = blockArrayMaker(9, 5);
+const playerPad = { x: 20, y: 435, dx: 80, dy: 10 };
+blockArr.push(playerPad);
+blockArr.forEach((block) => {
+	blockBox(block.x, block.y, block.dx, block.dy, "dodgerblue");
 });
+
+// blockBox(20, 435, 80, 10, "black");
